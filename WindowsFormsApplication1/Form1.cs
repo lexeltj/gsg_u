0fa82ba7f72dcb5c5c1bde51546765d4df32bb20
lexeltj <lexel.tj@googlemail.com>
2015-11-13 10:06:50 +0100

    Quelltext bereinigt

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -24,65 +24,13 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            // Kleine Änderung im Quelltext
-            // Und noch eine Änderung mehr....
-            // Jetzt die dritte Änderung
-            this.button1.Text = "Update Starten";
-            //MessageBox.Show("Test");
-          /* int e3 =  myFunction();
-            if (e3 == 1)
-            {
-                MessageBox.Show("yes");
-            }
-            else
-            {
-                MessageBox.Show("no");
-            }
-            Console.Write("hallo welt");*/
-
-            //openFileDialog1.ShowDialog();
-            //label1.Text = openFileDialog1.FileName;
-
-            // Jetzt prüfen wir mal ob es ein bestimmtes Verzeichnis gibt
-            string arma3_32_d = @"C:\Program Files (x86)\Steam\steamapps\common\Arma 3\";
-            //string armavz;
-            /* string arma32 = @"C:\Program Files (x86)\Steam\steamapps\common\Arma 3\arma3.exe";
-             string folder86 = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
-             Console.WriteLine(folder86);
-             string arma64 = @"C:\Program Files\Steam\steamapps\common\Arma 3\arma3.exe"; ;
-             Console.WriteLine(File.Exists(arma32) ? "File exists." : "File does not exist.");
-             Console.WriteLine(File.Exists(arma64) ? "File exists." : "File does not exist.");
-             label1.ForeColor = Color.Blue;
-             label1.Text = arma32 + " " + (File.Exists(arma32) ? "File exists." : "File does not exist.") + "\n";
-             label1.Text += arma64 + " " + (File.Exists(arma64) ? "File exists." : "File does not exist.") + "\n";
-             label1.Text += (Directory.Exists(arma3_32_d) ? "Directory exists." : "Directory does not exist.") + ": ";
-             label1.Text += arma3_32_d + "\n\n";*/
-
-            /* if (File.Exists(arma32))
-             {
-                 label1.ForeColor = Color.Green;
-                 label1.Text += arma32 + " \n";
-                 armavz = arma32;
-             }
-             else
-             {
-                 label1.ForeColor = Color.Red;
-                 label1.Text += arma32 + "\n";
-                 armavz = arma64;
-             }
-             label1.Text = armavz;
-             */
-
-
-            // ------------ Version überprüfen --------------
-            //int vn = versionUeberpruefen();
-
-            //MessageBox.Show(vn.ToString());
-
-            // ------------ ARMA Verzeichnis suchen ---------
+           
+           // ------------ ARMA Verzeichnis suchen ---------
 
             string vz = sucheVerzeichnis();
             textBox1.Text += vz + " gefunden.\r\n";
+
+            // -------- Mod - Datei einlesen
             modDateiEinlesen(vz);
            
         }
@@ -94,7 +42,7 @@ namespace WindowsFormsApplication1
             string line;
             string[] mod = new string[1];
 
-            // ------ später -------
+            
             // Read the file and display it line by line.
             System.IO.StreamReader file =
                 new System.IO.StreamReader(@"C:\Program Files (x86)\Steam\steamapps\common\Arma 3\mods.txt");
@@ -117,14 +65,14 @@ namespace WindowsFormsApplication1
 
 
             textBox1.Text += "\r\n Read File: mods.txt success";
-            // textBox1.Text = label1.Text;
+           
 
-            //textBox1.Text += "\r\n" + "hallo welt";
             textBox1.SelectionStart = textBox1.Text.Length;
             textBox1.ScrollToCaret();
         }
         private string sucheVerzeichnis()
         {
+            // Jetzt prüfen wir mal ob es ein bestimmtes Verzeichnis gibt
             string arma32 = @"C:\Program Files (x86)\Steam\steamapps\common\Arma 3\";
             string arma64 = @"C:\Program Files\Steam\steamapps\common\Arma 3\";
 
@@ -149,60 +97,6 @@ namespace WindowsFormsApplication1
                 }
                 return arma_anders;
             }
-
-           
         }
-
-       /* private int versionUeberpruefen()
-        {
-            string arma32 = @"C:\Program Files (x86)\Steam\steamapps\common\Arma 3\";
-            string arma64 = @"C:\Program Files\Steam\steamapps\common\Arma 3\";
-
-            try
-            {
-                textBox1.Text += arma32 + (Directory.Exists(arma32) ? "Directory exists." : "Directory does not exist.");
-                return 32;
-            }
-            catch (Exception)
-            {
-
-                textBox1.Text += "ist kein 32 Bit System";
-                return 0;
-               
-            }
-
-            try
-            {
-                textBox1.Text += arma64 + (Directory.Exists(arma64) ? "Directory exists." : "Directory does not exist.");
-                return 64;
-            }
-            catch (Exception)
-            {
-                textBox1.Text += "ist kein 64 Bit System";
-                return 0;
-            }
-            
-
-            
-
-        }*/
-
-       /* private int  myFunction()
-        {
-             int e2;
-            
-            DialogResult result =  MessageBox.Show("Hallo", "Nachricht von lexel", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-
-            if (result == DialogResult.Yes)
-            {
-                e2 = 1;
-            }
-            else
-            {
-                e2 = 2;
-            }
-
-            return e2;
-        }*/
     }
 }
